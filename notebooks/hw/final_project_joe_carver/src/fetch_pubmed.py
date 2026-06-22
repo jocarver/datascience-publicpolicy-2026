5eabd8e736bd3c1957843303cdcdbcaf95885fee
jocarver <joseph.o.carver@gmail.com>
2026-06-22 18:24:35 +0900

    updated fetch
    updated fetch and retrieved Nature publications 2000-2026

--- notebooks/hw/final_project_joe_carver/src/fetch_pubmed.py
+++ notebooks/hw/final_project_joe_carver/src/fetch_pubmed.py
@@ -1,24 +1,7 @@
 from Bio import Entrez
-# import csv
 import pandas as pd
 from pathlib import Path
-# import git
-
-
-
-# ---------------------------------------------------------------
-# set up paths
-# ---------------------------------------------------------------
-# def get_repo_root() -> Path:
-#     """
-#     Locates the root directory of the current git repository.
-#     Returns a pathlib.Path object.
-#     """
-#     try:
-#         git_repo = git.Repo(".", search_parent_directories=True)
-#         return Path(git_repo.working_tree_dir).resolve()
-#     except git.exc.InvalidGitRepositoryError as e:
-#         raise RuntimeError("The current directory is not part of a Git repository") from e
+import time
 
 REPO_ROOT = Path(__file__).resolve().parents[1]
 RAW_DATA_DIR = REPO_ROOT / 'data' / 'raw'
@@ -27,7 +10,7 @@ RAW_DATA_DIR = REPO_ROOT / 'data' / 'raw'
 # get citations from pubmed using biopython entrez functionality
 # ---------------------------------------------------------------
 
-def get_scientific_citations(journal_name, year, csv_name, max_results=5,):
+def get_scientific_citations(journal_name, year, max_results=5,):
     Entrez.email = "jocarver@g.ecc.u-tokyo.ac.jp"
     
     # use [PT] (Publication Type) to target core research articles 
@@ -42,19 +25,13 @@ def get_scientific_citations(journal_name, year, csv_name, max_results=5,):
         f'NOT "Biography"[PT]'
     )
     
-    print(f"Searching PubMed with filters:\n{query}")
+    # print(f"Searching PubMed with filters:\n{query}\n")
     
     try:
         # 1. Search for IDs using the filtered query
         search_handle = Entrez.esearch(db="pubmed", term=query, retmax=max_results)
         search_results = Entrez.read(search_handle)
         search_handle.close()
-
-        total_count = int(search_results["Count"])
-        if total_count == 0:
-            print('No records found')
-            return
-
         
         pmid_list = search_results["IdList"]
         if not pmid_list:
@@ -69,15 +46,55 @@ def get_scientific_citations(journal_name, year, csv_name, max_results=5,):
         fetch_handle.close()
         
         # 3. Build dataframe from XML
-        citations = []
+        authors = []
+        pub_info = []
         for article in xml_data['PubmedArticle']:
             medline_citation = article['MedlineCitation']
             pmid = medline_citation['PMID']
             title = medline_citation['Article']['ArticleTitle']
             
-            # Optional: Grab the Publication Types from the XML to verify it's working
-            pub_types = medline_citation['Article']['PublicationTaskList'] if 'PublicationTaskList' in medline_citation['Article'] else []
-            pub_type_names = [str(pt) for pt in pub_types]
+            # get publication type
+            # pub_types = medline_citation['Article']['PublicationTaskList'] if 'PublicationTaskList' in medline_citation['Article'] else []
+            # pub_type_names = [str(pt) for pt in pub_types]
+
+            pub_type_names =medline_citation['Article'].get('PublicationTypeList', [])
+            if not pub_type_names:
+                continue
+
+            # get mesh terms
+            mesh_list = medline_citation.get('MeshHeadingList', [])
+            mesh_terms = []
+            for mesh in mesh_list:
+                term = str(mesh['DescriptorName'])
+                mesh_terms.append(term)
+
+            # get funding sources
+            grants = medline_citation['Article'].get('GrantList', [])
+            grant_ids = []
+            funding_agency = []
+            funding_country = []
+            for grant in grants:
+                grant_id = grant.get('GrantID', 'unknown_ID')
+                agency = grant.get('Agency', 'unknown_agency') 
+                country = grant.get('Country', 'unknown_country')
+                grant_ids.append(grant_id)
+                funding_agency.append(agency)
+                funding_country.append(country)
+
+            pub_info.append({
+                "pmid": pmid,
+                "journal": journal_name,
+                "title": title,
+                "year": year,
+                "pub_types": '|'.join(pub_type_names),
+                "mesh_terms":"|".join(mesh_terms),
+                "grant_ids": "|".join(grant_ids),
+                "funding_agencies": "|".join(funding_agency),
+                "funding_country": "|".join(funding_country)
+            })
+            #print(pub_info)
+
+
             
             # print(f"PMID: {pmid}")
             # print(f"TITLE: {title}")
@@ -98,33 +115,54 @@ def get_scientific_citations(journal_name, year, csv_name, max_results=5,):
                     if author.get('AffiliationInfo'):
                         for aff_info in author['AffiliationInfo']:
                             affiliations.append(aff_info['Affiliation'])
+                            # print(aff_info['Affiliation'])
 
-                    # Format affiliation string
-                    aff_string = " | ".join(affiliations) if affiliations else "No affiliation listed"
                     # print(f"  - {author_name}")
                     # print(f"    Affiliation: {aff_string}")
-                    citations.append({
+                    authors.append({
                         "PMID": pmid,
-                        "Title": title,
-                        "Pub types": '|'.join(pub_type_names),
-                        "Author": author_name if author_name else None,
-                        "Affiliations": "|".join(affiliations) if affiliations else "No affiliation"
-
+                        "author": author_name if author_name else None,
+                        "affiliations": "|".join(affiliations) if affiliations else ["No affiliation"]
                         })
             else:
                 continue
                 # print("  No authors listed.")
                 
             # print("-" * 50)
-        df = pd.DataFrame(citations)
-        print(f"Retrieved {len(df['Title'].unique())} publications with {len(df['Author'])} authors.")
-        print("Saving XML as output csv")
-        df.to_csv(RAW_DATA_DIR / csv_name)
+        pubs = pd.DataFrame(pub_info)
+        authors = pd.DataFrame(authors)
+        print(f"Retrieved {len(pubs['title'].unique())} publications with {len(authors['author'])} authors from {journal_name} in {year}")
 
+        return pubs, authors
+        # print("Saving XML as output csv")
+        # df.to_csv(RAW_DATA_DIR / csv_name)
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     # Test with a journal known for having lots of editorials/news (like Nature)
-    get_scientific_citations(journal_name="Nature", year=2024, max_results=1000, csv_name = "pubmed_citations.csv")
+
+    all_authors = []
+    all_pubs = []
+
+    for year in range(2000,2026):
+        time.sleep(2)
+        try:
+            pubs, authors = get_scientific_citations(journal_name = "Nature", year = year, max_results= 2000)
+            all_pubs.append(pubs)
+            all_authors.append(authors)
+        except Exception as e:
+            print(f"An error occured during the main loop: {e}")
+
+    final_pubs = pd.concat(all_pubs)
+    final_authors = pd.concat(all_authors)
+    csv_name_pub = f"Nature_2000-2026_pub_info.csv"
+    csv_name_author = f"Nature_2000-2026_author_info.csv"
+    final_pubs.to_csv(RAW_DATA_DIR / csv_name_pub)
+    final_authors.to_csv(RAW_DATA_DIR / csv_name_author)
+
+    # get_scientific_citations(journal_name="Nature", year=2024, max_results=10, csv_name = "pubmed_citations.csv")
+    # get_scientific_citations(journal_name="Cell", year=2024, max_results=2000, csv_name = "pubmed_citations.csv")
+
+
